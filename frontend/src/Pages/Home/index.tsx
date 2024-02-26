103463846d470c6b5b7e93836da712aedfa789f7
Lara Matos Aguirres <lara.laramatos@gmail.com>
2024-02-26 15:53:10 -0300

    001 -Project creation and home page

---
+++ frontend/src/Pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="content-container">
+        <h1>Meu portifolio</h1>
+        <h1>Meus projetos</h1>
+        <h1>Sobre mim</h1>
+        <h1>Me contate</h1>
+      </div>
+      <div className="social-container">
+        <h6>Instagram</h6>
+        <h6>Linkedin</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
