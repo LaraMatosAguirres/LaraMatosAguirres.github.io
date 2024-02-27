767c2fa5fdd5976b491c346413c443e8e9c64250
Lara Matos Aguirres <lara.laramatos@gmail.com>
2024-02-27 20:50:41 -0300

    CSS Home

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,17 +1,38 @@
+import { ReactComponent as Line } from "../../assets/images/Line.svg";
 import "./styles.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="home-container mw-100">
       <div className="content-container">
-        <h1>Meu portifolio</h1>
-        <h1>Meus projetos</h1>
-        <h1>Sobre mim</h1>
-        <h1>Me contate</h1>
+        <div>
+          <h1>Meus projetos</h1>
+        </div>
+        <div>
+          <h1>Sobre mim</h1>
+        </div>
+        <div>
+          <h1>Me contate</h1>
+        </div>
       </div>
-      <div className="social-container">
-        <h6>Instagram</h6>
-        <h6>Linkedin</h6>
+      <div className="specialization-content">
+        <div>
+          <h5>Meu container</h5>
+        </div>
+      </div>
+      <div className="nav-social-container">
+        <div className="nav-left-item">
+          <h6>Instagram</h6>
+        </div>
+        <div className="nav-left-item">
+          <h6>Linkedin</h6>
+        </div>
+        <div className="nav-left-item">
+          <Line />
+        </div>
+        <div className="nav-left-item nav-year-item">
+          <h1>/2024</h1>
+        </div>
       </div>
     </div>
   );
